9b99bf77575396a99fcfe7647cb82fb15591e88e
rishabhgupta97 <40624419+rishabhgupta97@users.noreply.github.com>
2018-06-28 06:25:56 -0600

    subtraction done

--- SimpleCalculation/src/simplecalculation/UseMe.java
+++ SimpleCalculation/src/simplecalculation/UseMe.java
@@ -5,7 +5,8 @@
  */
 package simplecalculation;
 
-import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,18 +17,22 @@ public class UseMe {
     public static double Addition(double x,double y){
         return x+y;
     }
+    public static double Subtraction(double x,double y){
+        return x-y;
+    }
     public static void main(String[] args) {
         try{
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter ist number");
-        double num1=scanner.nextDouble();
-        System.out.println("Enter 2nd number");
-        double num2=scanner.nextDouble();
+       
+        String s1=   JOptionPane.showInputDialog("Enter 1st number");
+        double num1=Integer.parseInt(s1);
+        String s2=   JOptionPane.showInputDialog("Enter 2nd number");
+        double num2=Integer.parseInt(s2);
         double add=UseMe.Addition(num1, num2);
         System.out.println("Sum of "+num1+" and "+num2+" numbers is "+add);
-    
+        double subtraction=UseMe.Subtraction(num1,num2);
+            System.out.println("Difference of "+num1+" and "+num2+" numbers is "+subtraction);
         }catch(Exception e){
-            System.out.println("Enter numbers only!!!!!!!!!!!!!!!!!");
+            JOptionPane.showMessageDialog( null, "Enter number only", "Failure", JOptionPane.ERROR_MESSAGE);
         }
     }
     
